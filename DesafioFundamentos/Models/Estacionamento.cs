449ab5ce6495011bd174184b4ab1929b378d723f
palomacanton <palomacanton04@gmail.com>
2023-12-24 12:16:31 -0300

    Método RemoverVeiculo() modificado no if

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -24,24 +24,23 @@ namespace DesafioFundamentos.Models
         {
             // Modificado
             Console.WriteLine("Digite a placa do veículo para remover:");
-            string placa = Console.ReadLine();
-            veiculos.Remove(placa);
-            
+            string placa = Console.ReadLine();            
 
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
-
+                
                 // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
+                int horas = int.Parse(Console.ReadLine());
+                
                 // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
                 // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
+                decimal valorTotal = (precoInicial + precoPorHora) * horas; 
 
                 // TODO: Remover a placa digitada da lista de veículos
                 // *IMPLEMENTE AQUI*
-
+                veiculos.Remove(placa);
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
             }
             else
